3a37a8c3e344ede751f4950c88bf27653f36575f
AnuradhaNE <adn24@njit.edu>
2017-04-09 23:16:38 -0400

    Model Ready

--- xampp/htdocs/final/model/people.php
+++ xampp/htdocs/final/model/people.php
@@ -1,8 +1,32 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require('database.php');
+ /***Author :Anu*/
 
+/* File to deal with the members dbase of this app*/
+function register($email,$fname,$lname,$pword,$cell_phone_no,$bday,$gender) {
+   global $db;
+   $q=" INSERT INTO `to_do_members` (`email`, `first_n`, `last_n`, `pword`, `cell_phone_no`, `b_day`, `gender`) VALUES ('$email', '$fname', '$lname', '$pword', '$cell_phone_no', '$bday', '$gender')";
+            
+$statement = $db->prepare($q);
+ $statement->execute();
+ $statement->closeCursor();
+ return ;
+}
+function validate($email,$pword) {
+   global $db;
+   $q="SELECT `first_n` FROM `adn24`.`to_do_members` where email='$email' and pword='$pword'";
+          
+$statement = $db->prepare($q);
+ $statement->execute();
+   $category = $statement->fetch();
+   print_r(count($statement->fetchAll()));
+
+    $statement->closeCursor();    
+    $f_name = $category['first_n'];
+    echo $f_name;
+    echo 'is'.$f_name==='';
+    return $f_name;
+}
+//register('ggj@@');
+?>
--- xampp/htdocs/final/model/tasks.php
+++ xampp/htdocs/final/model/tasks.php
@@ -1,8 +1,33 @@
 <?php
+require('database.php');
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/***Author :Anu*/
 
+/* File to deal with the tasks of this app*/
+function  getTasksFor($email)
+{
+    
+    global $db;
+    $query = "SELECT * FROM `adn24`.`to_do_tasks` WHERE email='$email' ORDER BY `uploaded_on` DESC";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    return $statement;    
+
+}
+function putaTask($task_title, $task_desc, $email, $task_date, $task_time, $status)
+{
+     global $db;
+   $q=" INSERT INTO `adn24`.`to_do_tasks` (`task_title`, `task_desc`, `email`, `task-date`, `task-time`, `status`) VALUES ('$task_title', '$task_desc', '$email', '$task_date', '$task_time', '$status')
+ ";
+            
+$statement = $db->prepare($q);
+ $statement->execute();
+ $statement->closeCursor();
+ return ;
+}
+?>
