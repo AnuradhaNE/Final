bbc01f490e5278066a83e3e8d911198fdddd9b4b
AnuradhaNE <adn24@njit.edu>
2017-05-01 21:58:45 -0400

    add input resizing

--- xampp/htdocs/final/view/header.php
+++ xampp/htdocs/final/view/header.php
@@ -33,8 +33,14 @@ input[type="submit"] {
     color: white;
     border: none;
     border-radius: 5px;
+    cursor:pointer;
 }
-
+input[disabled="disabled"] {
+    cursor: not-allowed;
+}
+input[type="submit",disabled="disabled"]{
+    cursor: not-allowed;
+}
 h2 {
     font-weight: bold;
 }
@@ -120,13 +126,14 @@ a:hover {
     transition: 0.2s;
 }
 
-#reg input[type="text"],#reg input[type="radio"], #reg select {
+#reg input[type="text"],#reg input[type="radio"],#reg input[type="password"],#reg input[type="email"],#reg input[type="tel"],#reg input[type="date"], #reg select {
     border: 5px double #9c7f7f;
     /* text-align: right; */
     /* float: right; */
     margin-top: 23px;
     outline: none;
 min-width: 30px;}
+    
     
     
 
