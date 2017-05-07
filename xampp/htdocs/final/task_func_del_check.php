2ef2c404c5c9c463a5fa35931a27637019acd4f4
AnuradhaNE <adn24@njit.edu>
2017-05-07 03:52:51 -0400

    update prof+pass

--- xampp/htdocs/final/task_func_del_check.php
+++ xampp/htdocs/final/task_func_del_check.php
@@ -1,7 +1,9 @@
 <?php
-require 'model/tasks.php';
-       date_default_timezone_set('America/New_York');
 
+require 'model/tasks.php';
+require 'model/people.php';
+       date_default_timezone_set('America/New_York');
+session_start();
 if(isset($_REQUEST['f'])&&isset($_REQUEST['email'])&&isset($_REQUEST['tasktitle'])){
     
     if($_REQUEST['f']=='d'){
@@ -20,6 +22,21 @@ if(isset($_REQUEST['f'])&&isset($_REQUEST['email'])&&isset($_REQUEST['tasktitle'
     }
     header('location:main.php');
 }
+else  if(isset($_POST['first_name'])&&isset($_POST['mobile_no'])&&isset($_SESSION['email'])){
+        
+        updateUser($_POST['first_name'],$_POST['last_name'],$_POST['mobile_no'],$_POST['bday'],$_POST['G'],$_SESSION['email']);
+         header('location:main.php');
+        //echo '<script>window.location.reload();</script>';
+
+        }
+
+else  if(isset($_POST['opword'])&&isset($_POST['pword'])&&isset($_SESSION['email'])){
+        
+        updatepass($_POST['pword'],$_SESSION['email'],$_POST['opword']);
+         header('location:main.php');
+        //echo '<script>window.location.reload();</script>';
+
+        }
 else {
  
     header('location:errors/error.php');
